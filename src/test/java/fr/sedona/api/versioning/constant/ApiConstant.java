65de9232c87871b914a2dd61dbe78d83cc156a58
Edouard lamotte <elamotte@sedona.fr>
2022-09-27 12:06:11 +0200

    add rest client

--- src/test/java/fr/sedona/api/versioning/constant/ApiConstant.java
+++ src/test/java/fr/sedona/api/versioning/constant/ApiConstant.java
@@ -1,8 +1,8 @@
 package fr.sedona.api.versioning.constant;
 
 public class ApiConstant {
-    public static final String API_URI_BEERS_V1 = "/v1/beers";
-    public static final String API_URI_BREWERIES_V1 = "/v1/breweries";
-    public static final String API_URI_BEERS_V2 = "/v2/beers";
-    public static final String API_URI_BREWERIES_V2 = "/v2/breweries";
+    public static final String API_URI_BEERS_V1 = "/api/v1/beers";
+    public static final String API_URI_BREWERIES_V1 = "/api/v1/breweries";
+    public static final String API_URI_BEERS_V2 = "/api/v2/beers";
+    public static final String API_URI_BREWERIES_V2 = "/api/v2/breweries";
 }
