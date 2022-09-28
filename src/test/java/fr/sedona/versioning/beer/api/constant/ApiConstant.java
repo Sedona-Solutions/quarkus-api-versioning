8799f4a70717ad81c2822c92851c6d6390755e7e
Edouard lamotte <elamotte@sedona.fr>
2022-09-28 15:15:12 +0200

    add RestApiApplication

--- src/test/java/fr/sedona/versioning/beer/api/constant/ApiConstant.java
+++ src/test/java/fr/sedona/versioning/beer/api/constant/ApiConstant.java
@@ -2,8 +2,8 @@ package fr.sedona.versioning.beer.api.constant;
 
 public class ApiConstant {
     public static final String HEADER_ACCEPT = "Accept";
-    public static final String API_URI_BEERS = "/beers";
-    public static final String API_URI_BREWERIES = "/breweries";
+    public static final String API_URI_BEERS = "/api/beers";
+    public static final String API_URI_BREWERIES = "/api/breweries";
     public static final String API_HEADER_V1 = "application/vnd.company.app-v1+json";
     public static final String API_HEADER_V2 = "application/vnd.company.app-v2+json";
 }
